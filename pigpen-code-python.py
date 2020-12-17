0f18d79cc56878a3bf45b08d38171a66461c9b28
Jake Freeland <70768101+jakesfreeland@users.noreply.github.com>
2020-12-17 17:54:26 -0600

    Update pigpen-code-python.py
    fixed

--- pigpen-code-python.py
+++ pigpen-code-python.py
@@ -1,35 +1,40 @@
-def key():
-  a="_|"
-  b="|_|"
-  c="|_"
-  d="]"
-  e="[]"
-  f="["
-  g="-|"
-  h="|-|"
-  i="|-"
-  j="._|"
-  k="|._|"
-  l="|._"
-  m=".]"
-  n="[.]"
-  o="[."
-  p=".-|"
-  q="|.-|"
-  r="|.-"
-  s="v"
-  t=">"
-  u="<"
-  v="^"
-  w=".v"
-  x=".>"
-  y=".<"
-  z=".^"
-
 def main():
-message = input()
-for letter in message:
-   letter = letter.lower()
-   message.append(key.get(letter, letter))
-   return ''.join(message)
-main() 
+    key = {
+    "a":"_|",
+    "b":"|_|",
+    "c":"|_",
+    "d":"]",
+    "e":"[]",
+    "f":"[",
+    "g":"-|",
+    "h":"|-|",
+    "i":"|-",
+    "j":"._|",
+    "k":"|._|",
+    "l":"|._",
+    "m":".]",
+    "n":"[.]",
+    "o":"[.",
+    "p":".-|",
+    "q":"|.-|",
+    "r":"|.-",
+    "s":"v",
+    "t":">",
+    "u":"<",
+    "v":"^",
+    "w":".v",
+    "x":".>",
+    "y":".<",
+    "z":".^",
+    ".":"\n",
+    " ":" "
+}
+    print("Input Message:")
+    message = input()
+    encr = ""
+    for letter in message:
+        letter = letter.lower()
+        if letter in key:
+            encr += key[letter] + " "
+    print(encr)
+main()
